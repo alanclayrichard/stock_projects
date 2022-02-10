c36a7c5d14b947b434c7fb40e46fc937875778b3
Clay Richard <alanclayrichard@gmail.com>
2022-02-10 16:03:07 -0600

    added linear and quadratic regression functionality

--- stock_regression.py
+++ stock_regression.py
@@ -1,23 +1,26 @@
-from typing import OrderedDict
 from matplotlib import pyplot as plt
 import yfinance as yf
 import pandas as pd
 import numpy as np
-from urllib import request
-# from sklearn.linear_model import LinearRegression
 
-# input tickers
+# input tickers to analyze and yfinance formatted time period (10y 2mo etc)
 ticker1 = "MSFT"
 ticker2 = "TSLA"
+time_period = '10y'
 
-# get data
+# get data for tickerts
 get_data1= yf.Ticker(ticker1)
 get_data2 = yf.Ticker(ticker2)
-ticker1_data = get_data1.history(period='10y')
-ticker2_data = get_data2.history(period='10y')
+ticker1_data = get_data1.history(period=time_period)
+ticker2_data = get_data2.history(period=time_period)
+# data will be closing prices for this model
 close_prices1 = ticker1_data.iloc[:,3].to_numpy()
 close_prices2 = ticker2_data.iloc[:,3].to_numpy()
+# create an index to regress onto and plot and then create a prediction index to model future behavior 100 days into the future
 index = np.linspace(0,len(close_prices1),len(close_prices1))
+pred_index = np.linspace(0,len(close_prices1)+100,len(close_prices1)+100)
+
+# # get recent prices
 # tick1_text = ticker1 + " is at " + str(close_prices1[-1]) + " most recently"
 # tick2_text = ticker2 + " is at " + str(close_prices2[-1]) + " most recently"
 # print(tick1_text)
@@ -29,8 +32,8 @@ index = np.linspace(0,len(close_prices1),len(close_prices1))
 # print(ticker1 + ' Market Cap: '+ str(ticker1_marketCap))
 # print(ticker2 + ' Market Cap: '+ str(ticker2_marketCap))
 
-# plot 
-plt.figure(figsize=[10,4])
+# plot share price
+plt.figure(figsize=[14,6])
 plt.plot(index,close_prices1)
 plt.plot(index,close_prices2)
 plt.xticks(size=4,rotation=45)
@@ -40,39 +43,48 @@ xhat1 = np.average(index)
 yhat1 = np.average(close_prices1)
 beta11 = np.sum((close_prices1-yhat1)*(index-xhat1))/np.sum((index-xhat1)**2)
 beta01 = yhat1 - beta11*xhat1
-pred1 = beta11*index + beta01
-print(beta11)
-print(beta01)
-plt.plot(index,pred1)
+pred1 = beta11*pred_index + beta01
+# print(beta11)
+# print(beta01)
+plt.plot(pred_index,pred1)
 
 #linear model for 2
 xhat2 = np.average(index)
 yhat2 = np.average(close_prices2)
 beta12 = np.sum((close_prices2-yhat2)*(index-xhat2))/np.sum((index-xhat2)**2)
 beta02 = yhat2 - beta12*xhat2
-pred2 = beta12*index + beta02
+pred2 = beta12*pred_index + beta02
 print(beta12)
 print(beta02)
-plt.plot(index,pred2)
+plt.plot(pred_index,pred2)
+
+# quadtratic model for 1
+sum_matrix1 = np.array([np.sum(index**4), np.sum(index**3), np.sum(index**2), np.sum(index**3), np.sum(index**2), np.sum(index), np.sum(index**2), np.sum(index), len(index)])
+sum_matrix1 = np.reshape(sum_matrix1,[3,3])
+# print(sum_matrix)
+sum_solutions1 = (np.array([np.sum(np.multiply(index**2,close_prices1)),np.sum(np.multiply(index,close_prices1)),np.sum(close_prices1)]))
+sum_solutions1 = np.reshape(sum_solutions1,[3,1])
+print(sum_solutions1)
+betas1 = np.dot(np.linalg.inv(sum_matrix1),sum_solutions1)
+print(betas1)
+quad_prediction1 = betas1[2] + betas1[1]*pred_index + betas1[0]*(pred_index**2)
+plt.plot(pred_index,quad_prediction1)
+
+# quadratic model for 2
+sum_matrix2 = np.array([np.sum(index**4), np.sum(index**3), np.sum(index**2), np.sum(index**3), np.sum(index**2), np.sum(index), np.sum(index**2), np.sum(index), len(index)])
+sum_matrix2 = np.reshape(sum_matrix2,[3,3])
+# print(sum_matrix2)
+sum_solutions2 = (np.array([np.sum(np.multiply(index**2,close_prices2)),np.sum(np.multiply(index,close_prices2)),np.sum(close_prices2)]))
+sum_solutions2 = np.reshape(sum_solutions2,[3,1])
+print(sum_solutions2)
+betas2 = np.dot(np.linalg.inv(sum_matrix2),sum_solutions2)
+print(betas2)
+quad_prediction2 = betas2[2] + betas2[1]*pred_index + betas2[0]*(pred_index**2)
+plt.plot(pred_index,quad_prediction2)
+
+# format plot
 plt.title(ticker1 + " and " + ticker2 + " Analysis")
 plt.ylabel("share price")
-plt.legend([ticker1,ticker2,ticker1+" linear model",ticker2+" linear model"])
+plt.xlabel("previous " + time_period)
+plt.legend([ticker1,ticker2,ticker1+" linear model",ticker2+" linear model",ticker1+" quadratic model",ticker2+" quadratic model"])
 plt.show()
-
-
-# # Treasury Data
-# data = pd.read_csv("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/avg_interest_rates?fields=record_date,security_desc,avg_interest_rate_amt&filter=record_date:gte:2021-12-01&format=csv")
-# dates = data['record_date']
-# description = data['security_desc']
-# rates = data['avg_interest_rate_amt']
-# print(len(dates))
-# print(len(description))
-# print(len(rates))
-# print(dates,description,rates)
-
-# data.plot()
-# # # data = np.array(data,[:,2])
-# # # print(data)
-# # plt.plot(data[0],data[1])
-# # plt.show()
-# plt.show()
